526c931758d7b4ce9113483eddac3690d44379f3
Goyo <goyodiaz@gmail.com>
2022-03-21 00:17:01 +0100

    Allow local data files.

--- st_app.py
+++ st_app.py
@@ -1,27 +1,47 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+
+
+def load_data():
+    data_src = st.radio('Data source', options=('URL', 'Local file'), index=1)
+    if data_src == 'URL':
+        return load_from_url()
+    elif data_src == 'Local file':
+        return load_from_file()
+
+
+def load_from_url():
+    default_url = 'https://www.sanidad.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Datos_Capacidad_Asistencial_Historico_18032022.csv'
+    url = st.text_input('Data URL')
+    default_url
+    return parse_data(url)
+
+
+def load_from_file():
+    file = st.file_uploader('Data file', type='csv')
+    return parse_data(file)
 
 
 @st.cache
-def load_data():
-    DATA_URL = 'https://www.sanidad.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Datos_Capacidad_Asistencial_Historico_18032022.csv'
-    data = pd.read_csv(DATA_URL, sep=';', encoding='latin')
+def parse_data(io):
+    if not io:
+        return None
+    data = pd.read_csv(io, sep=';', encoding='latin')
     data['Fecha'] = pd.to_datetime(data['Fecha'], format='%d/%m/%Y')
     return data
 
 
 @st.cache
 def get_regions(data):
-    col_name = 'CCAA'
-    values = data[col_name].unique()
-    values.sort()
-    return values
+    return get_unique_values(data, col_name='CCAA')
 
 
-@st.cache
+# ~ @st.cache
 def get_units(data):
-    col_name = 'Unidad'
+    return get_unique_values(data, col_name='Unidad')
+
+
+def get_unique_values(data, col_name):
     values = data[col_name].unique()
     values.sort()
     return values
@@ -31,15 +51,16 @@ def altair_chart(chart_data):
     st.line_chart(chart_data)
 
 
+def occupation_by_region(data):
+    ca = st.selectbox('Comunidad', get_regions(data), key=0)
+    chart_data = data.query(f'CCAA == \'{ca}\'').groupby(['Fecha', 'Unidad'])['OCUPADAS_COVID19'].sum().unstack(level='Unidad')
+    st.area_chart(chart_data.rename_axis(columns=None))
+    st.write(chart_data.assign(**{'Total ocupadas COVID-19': chart_data.sum(axis='columns')}))
+
+
 st.title('Capacidad asistencial')
+st.write('Ocupación de camas hospitalarias por COVID-19')
 data = load_data()
-
-st.subheader('Datos')
-st.write(data)
-
-left, right = st.columns(2)
-ca = left.selectbox('Comunidad', get_regions(data))
-unit = right.selectbox('Unidad', get_units(data))
-
-chart_data = data.query(f'CCAA == \'{ca}\' & Unidad == \'{unit}\'').groupby('Fecha')['OCUPADAS_COVID19'].sum()
-altair_chart(chart_data)
+if data is None:
+    st.stop()
+occupation_by_region(data)
